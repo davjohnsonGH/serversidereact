475ca79f19c28f3abf3f0150da68f1f8df738e59
David Johnson <davidjohnson@Davids-MacBook-Pro.local>
2020-02-23 23:58:45 -0800

    adds base and merge webpack.

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
+const config = {
     // Inform webpack that we are building a bundle for node, rather than in the browser.
     target: 'node',
     // Tell webpack the root file of our application.
@@ -9,22 +11,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    },
-    // Tell webpack to run babel on every file it runs through.
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        'react',
-                        'stage-0',
-                        ['env', {targets: {browsers: ['last 2 versions'] }}]
-                    ]
-                }
-            }
-        ]
-    },
+    }
 };
+
+module.exports = merge(baseConfig, config);
