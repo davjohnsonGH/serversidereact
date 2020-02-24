9a321081ad2dbbf99ed5ff2631be25706c733214
David Johnson <davidjohnson@Davids-MacBook-Pro.local>
2020-02-24 15:05:00 -0800

    users list component.

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './component/Home';
+import UsersList from './component/UsersList';
 
 export default () => {
     return (
         <div>
             <Route exact path="/" component={Home} />
+            <Route path="/users" component={UsersList} />
         </div>
     );
 };
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -7,7 +7,7 @@ import Routes from '../client/Routes';
 export default (req, store) => {
     const content = renderToString(
         <Provider store={store}>
-            <StaticRouter location={req.path} content={{}}>
+            <StaticRouter location={req.path} context={{}}>
                 <Routes />
             </StaticRouter>   
         </Provider>   
