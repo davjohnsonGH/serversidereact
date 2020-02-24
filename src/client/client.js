f7025ed3f91fdeeb709e312e91a8af505fad7c59
David Johnson <davidjohnson@Davids-MacBook-Pro.local>
2020-02-24 00:43:40 -0800

    routing.

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,14 @@
 // start up point for client side app
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './component/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDOM.hydrate(<Home />, document.getElementById('root'));
+ReactDOM.hydrate(
+
+    <BrowserRouter>
+        <Routes/>
+    </BrowserRouter>, 
+    document.getElementById('root')
+    
+);
