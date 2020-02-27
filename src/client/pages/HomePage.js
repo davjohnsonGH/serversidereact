b144529cbcd4ec0ef943dc1bce6d7c63ba510c33
David Johnson <davidjohnson@Davids-MacBook-Pro.local>
2020-02-26 23:56:50 -0800

    more styles.

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <div>Homeward bound</div>
-            <button onClick={ () => console.log('hello world') }>click on me</button>
+        <div className="center-align" style={{marginTop: '200px'}}>
+            <h3>Welcome</h3>
+            <p>check out ssr</p>
         </div>
     )
 };
