1ccc89ffb229f3afa22743d9eaa35772c29587cf
David Johnson <davidjohnson@Davids-MacBook-Pro.local>
2020-02-26 23:11:04 -0800

    auth and nav with global app class.

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import UsersListPage, { loadData } from './pages/UsersListPage';
+import App from './App';
 // react router config
 export default [
     {
-        ...HomePage,
-        path: '/',
-        exact: true
-    },
-    {   
-        ...UsersListPage,
-        path: '/users'
+        ...App,
+        routes: [
+            {
+                ...HomePage,
+                path: '/',
+                exact: true
+            },
+            {   
+                ...UsersListPage,
+                path: '/users'
+            }
+        ]
     }
 ];
+
+
